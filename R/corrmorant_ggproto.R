532818f6a58630bd0adc66a9dac0e2d164f6c787
r-link <romanlink@gmx.de>
2020-01-07 20:51:52 +0100

    Updated ggproto definitions with new stats

--- R/corrmorant_ggproto.R
+++ R/corrmorant_ggproto.R
@@ -5,21 +5,25 @@
 #' \itemize{
 #'   \item \code{StatCorrtext}: create and position text labels for correlation strength
 #'      (see \code{\link{stat_corrtext}} for details).
+#'   \item \code{StatHeatmap}: create correlation heat maps (see \code{\link{stat_heatmap}}
+#'      for details).
+#'   \item \code{StatHeatcircle}: create circles indicating correlation strength (see
+#'      \code{\link{stat_heatcircle}} for details).
 #'   \item \code{StatDiaNames}: create and position text labels for variable names in the
-#'      diagonal facets of \code{ggcorrm} plots (see \code{\link{dia_names}} for details).
+#'      diagonal facets of \code{ggcorrm} plots (see \code{\link{stat_dia_names}} for details).
 #'   \item \code{StatDiaDensity}: create and position density plots of variables in the
-#'      diagonal facets of \code{ggcorrm} plots (see \code{\link{dia_density}} for details).
+#'      diagonal facets of \code{ggcorrm} plots (see \code{\link{stat_dia_density}} for details).
 #'   \item \code{StatDiaBin}: create and position plots of binned data (histograms or
 #'     frequency polygons) of variables in the diagonal facets of \code{ggcorrm} plots
-#'     (see \code{\link{dia_histogram}} and  \code{\link{dia_freqpoly}}
-#'     for details).
+#'     (see \code{\link{stat_dia_bin}} for details).
 #'   }
 #' @seealso
 #'  \code{\link[ggplot2]{ggplot2-ggproto}},
-#'  \code{\link{stat_corrtext}},
+#'  \code{\link{geom_corrtext}},
+#'  \code{\link{geom_heatmap}},
+#'  \code{\link{geom_heatcircle}},
 #'  \code{\link{dia_names}},
 #'  \code{\link{dia_density}},
-#'  \code{\link{dia_histogram}},
-#'  \code{\link{dia_freqpoly}}
+#'  \code{\link{dia_histogram}}
 #' @name corrmorant_ggproto
 NULL
