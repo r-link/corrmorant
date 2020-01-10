add3f12858b28bcf898670d6b7a13b95a49b1c25
r-link <romanlink@gmx.de>
2020-01-10 19:18:19 +0100

    Remove purrr dependency
    * purrr::map() was only called one single time in the entire package - now replaced by regular lapply() call

--- R/tidy_corrm.R
+++ R/tidy_corrm.R
@@ -2,74 +2,74 @@
 
 #' @title Reshape data for correlation matrices to a tidy format
 #'
-#' @description `tidy_corrm()` takes a `data.frame` or `matrix`
-#'   and reshapes it to a long-table format that enables plotting with
-#'   [ggcorrm()].
+#' @description `tidy_corrm()` takes a `data.frame` or `matrix` and reshapes it
+#'   to a long-table format that enables plotting with [ggcorrm()].
 #'
-#' @param data `data.frame` or `matrix` with the raw data used
-#'   for the correlation plot. If a `data.frame`, all numeric variables
-#'   are used as rows/columns of the correlation plot, while all other
-#'   variables are appended to the reshaped dataset as additional columns.
+#' @param data `data.frame` or `matrix` with the raw data used for the
+#'   correlation plot. If a `data.frame`, all numeric variables are used as
+#'   rows/columns of the correlation plot, while all other variables are
+#'   appended to the reshaped dataset as additional columns.
 #' @inheritParams ggcorrm
-#' @return An object of class `tidy_corrm` (a tibble with structured
-#'   correlation data) containing the following columns:
-#'   \describe{
-#'      \item{var_x}{Name of the variable on the x-axis in
-#'        the order of appearance in the raw data (ordered factor).}
-#'      \item{var_y}{Name of the variable on the y-axis in
-#'        the order of appearance in the raw data (ordered factor)}
-#'      \item{x}{Data of the variable on the x axis (numeric).}
-#'      \item{y}{Data of the variable on the y axis (numeric).}
-#'      \item{type}{Type of panel (character, `"upper"`, `"lower"`` or
-#'        `"diag"`}).}
-#'      \item{.corr}{Correlation between x and y for the respective
-#'        panel/group, calculated with [cor()][stats::cor] using
-#'        the method specified by `corr_method` and optionally
-#'        within the groups specified with `corr_group` (numeric).}
-#'      \item{corr_group}{grouping variable for `.corr` (1 for all
-#'      observations if no groups are specified).}
-#'      \item{Additional columns}{All other columns specified in the dataset
-#'        and/or created via `mutates`.}
-#'      }
+#' @return An object of class `tidy_corrm` (a tibble with structured correlation
+#'   data) containing the following columns:
 #'
-#' @details `tidy_corrm()` is used to reshape a `data.frame` or
-#'   `matrix` with raw data for a correlation plot to a long-table
-#'   format that can be plotted with [ggcorrm()]. The function
-#'   creates a tibble with all combinations of all numeric variables in the
-#'   dataset that are labelled with their column names (or, alternatively,
-#'   a vector with new labels) in the order of their appearance in the raw
-#'   data. All other variables are appended to the reshaped data.frame and
-#'   can be accessed in the plots.
+#'   \describe{ \item{var_x}{Name of the variable on the x-axis in the order of
+#'   appearance in the raw data (ordered factor).}
 #'
-#'   By default, the data are scaled and centered using their standard
-#'   deviation (`rescale = "by.sd"`), but it is also possible to
-#'   rescale them into the range from 0 to 1 (`rescale = "by.range"`) or
-#'   to maintain the original scale of the data (`rescale = NULL`).
+#'   \item{var_y}{Name of the variable on the y-axis in the order of appearance
+#'   in the raw data (ordered factor)}
 #'
-#'   An additional variable called `.corr` with the bivariate correlation
-#'   of the two variables (by default, Pearson correlation,
-#'   see [cor()][stats::cor]) is appended to the dataset. This variable
-#'   can e.g. be used to specify the colour or fill of geoms conditional of
-#'   the strength of the correlation (see examples in [ggcorrm()]).
-#'   If the correlations displayed with [lotri_corrtext()] or
-#'   [utri_corrtext()] are separated
-#'   by groups, it may make sense to also calculate `.corr` separately for
-#'   these groups. In this case, it is possible to specify a grouping variable
-#'   for the calculation of `.corr` using `corr_group`.
+#'   \item{x}{Data of the variable on the x axis (numeric).}
 #'
-#'   In many cases, the variables in the correlation matrix belong to
-#'   different groups of variables. As the input for `tidy_corrm()` is
-#'   based on a wide table format, it is usually not possible to include this
-#'   information as an additional column in the raw data. There are two ways to
-#'   include variable-specific information _after the fact_: a)
-#'   `tidy_corrm()` can be called directly, and its output can be modified
-#'   manually before passing it to `ggcorrm()` or b) the `mutates`
-#'   argument can be used to pass a list of named quosures created with
-#'   [rlang::quos()] that contain a set of mutating
-#'   operations based on regular [dplyr::mutate()]
-#'   syntax that are evaluated inside the reshaped dataset (see examples).
-#'   For the standard column names of `tidy_corr` objects see the Value
-#'   section.
+#'   \item{y}{Data of the variable on the y axis (numeric).}
+#'
+#'   \item{type}{Type of panel (character, `"upper"`, `"lower"`` or `"diag"`).}
+#'
+#'   \item{.corr}{Correlation between x and y for the respective panel/group,
+#'   calculated with [cor()][stats::cor] using the method specified by
+#'   `corr_method` and optionally within the groups specified with `corr_group`
+#'   (numeric).}
+#'
+#'   \item{corr_group}{grouping variable for `.corr` (1 for all observations if
+#'   no groups are specified).}
+#'
+#'   \item{Additional columns}{All other columns specified in the dataset and/or
+#'   created via `mutates`.} }
+#'
+#' @details `tidy_corrm()` is used to reshape a `data.frame` or `matrix` with
+#'   raw data for a correlation plot to a long-table format that can be plotted
+#'   with [ggcorrm()]. The function creates a tibble with all combinations of
+#'   all numeric variables in the dataset that are labelled with their column
+#'   names (or, alternatively, a vector with new labels) in the order of their
+#'   appearance in the raw data. All other variables are appended to the
+#'   reshaped data.frame and can be accessed in the plots.
+#'
+#'   By default, the data are scaled and centered using their standard deviation
+#'   (`rescale = "by.sd"`), but it is also possible to rescale them into the
+#'   range from 0 to 1 (`rescale = "by.range"`) or to maintain the original
+#'   scale of the data (`rescale = NULL`).
+#'
+#'   An additional variable called `.corr` with the bivariate correlation of the
+#'   two variables (by default, Pearson correlation, see [cor()][stats::cor]) is
+#'   appended to the dataset. This variable can e.g. be used to specify the
+#'   colour or fill of geoms conditional of the strength of the correlation (see
+#'   examples in [ggcorrm()]). If the correlations displayed with
+#'   [lotri_corrtext()] or [utri_corrtext()] are separated by groups, it may
+#'   make sense to also calculate `.corr` separately for these groups. In this
+#'   case, it is possible to specify a grouping variable for the calculation of
+#'   `.corr` using `corr_group`.
+#'
+#'   In many cases, the variables in the correlation matrix belong to different
+#'   groups of variables. As the input for `tidy_corrm()` is based on a wide
+#'   table format, it is usually not possible to include this information as an
+#'   additional column in the raw data. There are two ways to include
+#'   variable-specific information _after the fact_: a) `tidy_corrm()` can be
+#'   called directly, and its output can be modified manually before passing it
+#'   to `ggcorrm()` or b) the `mutates` argument can be used to pass a list of
+#'   named quosures created with [rlang::quos()] that contain a set of mutating
+#'   operations based on regular [dplyr::mutate()] syntax that are evaluated
+#'   inside the reshaped dataset (see examples). For the standard column names
+#'   of `tidy_corr` objects see the Value section.
 #'
 #' @examples
 #' \dontrun{
@@ -92,16 +92,14 @@
 #'    head(corrdat2)
 #'  }
 #' }
-#' @seealso
-#' [ggcorrm],
-#' [corrmorant]
+#' @seealso [ggcorrm], [corrmorant]
 #'
 #' @rdname tidy_corrm
 #' @export
-#' @importFrom dplyr mutate_if mutate group_nest select arrange group_by ungroup case_when
+#' @importFrom dplyr mutate_if mutate group_nest select arrange group_by ungroup
+#'   case_when
 #' @importFrom stats sd
 #' @importFrom tidyr gather expand_grid unnest
-#' @importFrom purrr map
 tidy_corrm <- function(data,
                        labels = NULL,
                        rescale = c("by_sd", "by_range", NULL),
@@ -179,8 +177,8 @@ tidy_corrm <- function(data,
     # group by variable and nest
     dplyr::group_nest(var) %>%
     # add reduced nested dataset for y
-    dplyr::mutate(dat_y = purrr::map(data, ~ dplyr::select(.x, y = x))) %>%
-    # assure correct oder (important for correct subsetting - CAN BE DANGEROUS)
+    dplyr::mutate(dat_y = lapply(data, function(dat) dplyr::select(dat, y = x))) %>%
+    # assure correct oder (important for correct subsetting)
     dplyr::arrange(var)
 
   # prepare plotting output (all x and y and identifiers for everything else)
