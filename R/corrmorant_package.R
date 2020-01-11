bf4d50c13977672d50f08592d97b50807ff33335
Roman Link <romanlink@gmx.de>
2020-01-11 20:00:29 +0100

    Add package-level documentation
    * imported functions were moved from utils to this script

---
+++ R/corrmorant_package.R
@@ -0,0 +1,63 @@
+#' @title corrmorant - Flexible correlation matrices based on ggplot2
+#'
+#'@description corrmorant extends ggplot2 by an automated framework for plots of
+#'  correlation matrices that can be easily modified via regular ggplot2 syntax.
+#'  In addition, it provides a large set of visualization tools for exploratory
+#'  data analysis based on correlation matrices.
+#'
+#'@section Author:
+#'
+#'  **Roman M. Link** (Maintainer, [roman.link@plant-ecology.de])
+#'
+#'@section Where to start:
+#'
+#'  * [corrmorant()] - a simple, stripped down function for fast vizualization
+#'  of correlation matrices.
+#'  * [ggcorrm()] - the workhorse function for all corrmorant plots.
+#'  * [tidy_corrm()] - prepare datasets for plotting in corrmorant.
+#'  * [corrmorant_selectors] - functions that modify regular `ggplot2` layers to
+#'  direct them to the right positions in `ggcorrm()` plots: `dia()`, `lotri()`
+#'  and `utri()`.
+#'
+#'@section Special corrmorant layers:
+#'
+#'  **Modifying the plot diagonal**
+#'  * [dia_names()] - add text labels for variable names.
+#'  * [dia_density()] - add density plots.
+#'  * [dia_histogram()] - add histograms.
+#'  * [dia_freqpoly()] - add frequency polygons.
+#'  * [dia()] - add arbitrary ggplot layers to the diagonal.
+#'
+#'  **Modifying the lower triangle**
+#'  * [lotri_corrtext()] - add text labels for correlation strength.
+#'  * [lotri_funtext()] - add text labels for user-specified functions.
+#'  * [lotri_heatmap()] - add correlation heatmaps.
+#'  * [lotri_heatpoints()] - add symbols whose size and color indicates correlation strength.
+#'  * [lotri_heatcircle()] - add circles whose area scales with correlation strength.
+#'  * [lotri()] - add arbitrary ggplot layers to the lower triangle.
+#'
+#'  **Modifying the lower triangle**
+#'  * [utri_corrtext()] - add text labels for correlation strength.
+#'  * [utri_funtext()] - add text labels for user-specified functions.
+#'  * [utri_heatmap()] - add correlation heatmaps.
+#'  * [utri_heatpoints()] - add symbols whose size and color indicates correlation strength.
+#'  * [utri_heatcircle()] - add circles whose area scales with correlation strength.
+#'  * [utri()] - add arbitrary ggplot layers to the upper triangle.
+#'
+#'
+#' @docType package
+#'@name corrmorant_package
+NULL
+
+
+# import functions from other packages ----------------------------------------
+# entire ggplot2 package
+#' @import ggplot2
+
+# magrittr style pipelines
+#' @importFrom magrittr `%>%`
+NULL
+
+# some non-standard evaluation from rlang (probably only quos is needed)
+#' @importFrom rlang quo quos enquo enquos
+NULL
--- R/utils.R
+++ R/utils.R
@@ -75,16 +75,3 @@ modify_list <- function (old, new) {
 is.waive <- function (x) {
   inherits(x, "waiver")
 }
-
-# import functions from other packages ----------------------------------------
-# entire ggplot2 package
-#' @import ggplot2
-
-# magrittr style pipelines
-#' @importFrom magrittr `%>%`
-NULL
-
-# some non-standard evaluation from rlang (probably only quos is needed)
-#' @importFrom rlang quo quos enquo enquos
-NULL
-
