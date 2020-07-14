c9d3297e0f825e8c90eacc05b291db81eaf49299
r-link <romanlink@gmx.de>
2020-07-14 15:45:01 +0200

    Update default settings in corrmorant
    * New placement method for dia_names and dia_density
      makes it necessary to change the defaults to keep
      the plots looking good.

--- tests/testthat/test-corrmorant.R
+++ tests/testthat/test-corrmorant.R
@@ -10,8 +10,8 @@ test_that("corrmorant() and ggcorrm() create the same plots", {
   p2 <- ggcorrm(ldros, bg_dia = "grey20") +
     lotri(geom_point(alpha = min(1 / log10(nrow(ldros)), 1))) +
     utri_corrtext() +
-    dia_density(lower = .4, fill = "grey90", col = 1)+
-    dia_names(y_pos = .1, colour = "white", size = 3)
+    dia_density(lower = .3, fill = "grey90", col = 1)+
+    dia_names(y_pos = .15, colour = "white", size = 3)
 
   p1$plot_env <- p2$plot_env <- NULL
 
@@ -22,8 +22,8 @@ test_that("corrmorant() and ggcorrm() create the same plots", {
   p4 <- ggcorrm(ldros) +
     lotri(geom_point(alpha = min(1 / log10(nrow(ldros)), 1))) +
     utri_corrtext() +
-    dia_density(lower = .4, fill = "grey80", col = 1)+
-    dia_names(y_pos = .1, colour = "black", size = 3)
+    dia_density(lower = .3, fill = "grey80", col = 1)+
+    dia_names(y_pos = .15, colour = "black", size = 3)
 
   p3$plot_env <- p4$plot_env <- NULL
 
@@ -34,8 +34,8 @@ test_that("corrmorant() and ggcorrm() create the same plots", {
   p6 <- ggcorrm(ldros) +
     lotri(geom_point( aes(col = .corr), alpha = min(1 / log10(nrow(ldros)), 1))) +
     utri_corrtext(aes(col = .corr)) +
-    dia_density(lower = .4, fill = "grey80", col = 1) +
-    dia_names(y_pos = .1, colour = "black", size = 3) +
+    dia_density(lower = .3, fill = "grey80", col = 1) +
+    dia_names(y_pos = .15, colour = "black", size = 3) +
     scale_color_corr()
 
   p5$plot_env <- p6$plot_env <- NULL
