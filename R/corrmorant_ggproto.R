0b05f67ea02a4b7ea7f168c8eabd9c4b9cd26130
r-link <romanlink@gmx.de>
2020-08-03 22:11:48 +0200

    Update ggproto documentation for geom_relpoint

--- R/corrmorant_ggproto.R
+++ R/corrmorant_ggproto.R
@@ -3,8 +3,8 @@
 #'@description Class definitions for the new `ggproto` classes defined in the
 #'  `corrmorant` package.
 #'
-#'@section New stats:
-#'  * `TidyCorrm`: prototype for [tidy_corrm()] constructor methods.
+#'@section New stats: * `TidyCorrm`: prototype for [tidy_corrm()] constructor
+#'  methods.
 #'
 #'  * `StatCorrtext`: create and position text labels for correlation strength
 #'  (see [stat_corrtext] for details).
@@ -26,30 +26,29 @@
 #'  (see [stat_dia_bin] for details).
 #'
 #'
-#'@section New geoms:
-#'  * `GeomReltext`: text labels with relative positions (see [geom_reltext] for
-#'  details).
-#'
-#'  * `GeomDiaDensity`: create and position density plots in the diagonal facets
-#'  of `ggcorrm` plots (see [geom_dia_density] for details).
+#'@section New geoms: * `GeomDiaDensity`: create and position density plots in
+#'  the diagonal facets of `ggcorrm` plots (see [geom_dia_density] for details).
 #'
 #'  * `GeomDiaHistogram`: create and position histogramsin the diagonal facets
 #'  of `ggcorrm` plots (see [geom_dia_density] for details).
 #'
-#'  * `GeomDiaFreqpoly`: create and position frequency polygons  in the diagonal
+#'  * `GeomDiaFreqpoly`: create and position frequency polygons in the diagonal
 #'  facets of `ggcorrm` plots (see [geom_dia_density] for details).
 #'
-#'@seealso
-#'  * [ggplot2-ggproto] for a description of the `ggproto` object orientation
-#'  system,
-#'  * [tidy_corrm] for structured tidy correlation plot datasets,
-#'  * [add_corrtext] for correlation text labels,
-#'  * [add_funtext] for user-defined text labels,
-#'  * [add_heatmap] for correlation heatmaps,
-#'  * [add_heatcircle] for circles witch correlation-dependent size and color,
-#'  * [dia_names] for variable names in the plot diagonal,
-#'  * [dia_density] for density plots in the plot diagonal,
-#'  * [dia_histogram] for histograms and frequency polygons in the plot diagonal
+#'  * `GeomRelpoint`: scatter plots with relative positions (see [geom_relpoint]
+#'  for details)-
+#'
+#'  * `GeomReltext`: text labels with relative positions (see [geom_reltext] for
+#'  details).
+#'
+#'@seealso * [ggplot2-ggproto] for a description of the `ggproto` object
+#'orientation system, * [tidy_corrm] for structured tidy correlation plot
+#'datasets, * [add_corrtext] for correlation text labels, * [add_funtext] for
+#'user-defined text labels, * [add_heatmap] for correlation heatmaps, *
+#'[add_heatcircle] for circles witch correlation-dependent size and color, *
+#'[dia_names] for variable names in the plot diagonal, * [dia_density] for
+#'density plots in the plot diagonal, * [dia_histogram] for histograms and
+#'frequency polygons in the plot diagonal
 #'
 #'@name corrmorant_ggproto
 NULL
