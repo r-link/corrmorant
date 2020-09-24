72661b8b387a2ba350cbd5e08227270be53f03ef
r-link <romanlink@gmx.de>
2020-09-25 00:20:45 +0200

    Add new corrmorant style for large datasets
    Closes #7

--- R/corrmorant.R
+++ R/corrmorant.R
@@ -5,14 +5,21 @@
 #'   values for initial data inspection.
 #'
 #' @param style Character string defining the plot style. One of `"dark"`,
-#'   `"light"` or `"blue_red"`.
+#'   `"light"`, `"blue_red"` or `"binned"`.
+#' @param bins Integer. Number of bins for `style = "binned"` (ignored if using
+#'   other styles). Defaults to 10.
 #' @param ... Additional arguments to [ggcorrm()].
 #' @inheritParams tidy_corrm
 #'
 #'
 #' @details `corrmorant()` is a simplified wrapper around [ggcorrm()] that
-#'   creates scatterplot matrices with reasonable standard settings. Refer to
-#'   the documentation of [ggcorrm()] and [tidy_corrm()] for details.
+#'   creates scatterplot matrices with reasonable standard settings. Setting the
+#'   `style` argument to `"dark"`, `"light"` or `"blue_red"` produces regular
+#'   scatterplots with different color schemes. `style = "binned"` replicates
+#'   `style = "blue_red"` with a binned scatterplot (the number of bins defaults
+#'   to 10, but can be changed with the `bins` argument), which can be very
+#'   useful to reduce the plotting time for large datasets. Refer to the
+#'   documentation of [ggcorrm()] and [tidy_corrm()] for more details.
 #'
 #' @examples
 #' \dontrun{
@@ -23,16 +30,15 @@
 #'  corrmorant(drosera, style = "blue_red")
 #'  }
 #' }
-#' @seealso
-#'   [ggcorrm],
-#'   [tidy_corrm]
+#' @seealso [ggcorrm], [tidy_corrm]
 #' @rdname corrmorant
 #' @export
 corrmorant <- function(data,
-                       style = c("blue_red", "dark", "light"),
+                       style = c("blue_red", "dark", "light", "binned"),
                        rescale     = c("as_is", "by_sd", "by_range"),
                        corr_method = c("pearson", "kendall", "spearman"),
                        labels      = NULL,
+                       bins = 10,
                        ...){
   # match arguments
   rescale     <- rlang::arg_match(rescale)
@@ -54,7 +60,7 @@ corrmorant <- function(data,
       lotri(
         geom_point(mapping = aes(col = .corr),
                    alpha   = min(1 / log10(nrow(data)), 1))
-        ),
+      ),
       utri_corrtext(mapping = aes(col = .corr)),
       dia_density(lower  = .3,
                   fill   = "grey80",
@@ -67,7 +73,7 @@ corrmorant <- function(data,
     dark = list(
       lotri(
         geom_point(alpha = min(1 / log10(nrow(data)), 1))
-        ),
+      ),
       utri_corrtext(),
       dia_density(lower = .3,
                   fill  = "grey90",
@@ -75,11 +81,11 @@ corrmorant <- function(data,
       dia_names(y_pos  = .15,
                 colour = "white",
                 size   = 3)
-      ),
+    ),
     light = list(
       lotri(
         geom_point(alpha = min(1 / log10(nrow(data)), 1))
-        ),
+      ),
       utri_corrtext(),
       dia_density(lower = .3,
                   fill  = "grey80",
@@ -87,6 +93,22 @@ corrmorant <- function(data,
       dia_names(y_pos  = .15,
                 colour = "black",
                 size   = 3)
+    ),
+    binned = list(
+      lotri(stat_bin2d(aes(col = .corr, size = after_stat(density)),
+                       geom = "point",
+                       alpha = 0.666,
+                       bins = bins,
+                       fill = NA,
+                       show.legend = c(size = FALSE, colour = TRUE))),
+      utri_corrtext(mapping = aes(col = .corr)),
+      dia_density(lower  = .3,
+                  fill   = "grey80",
+                  col    = 1),
+      dia_names(y_pos  = .15,
+                colour = "black",
+                size   = 3),
+      scale_color_corr()
     )
   )
 
